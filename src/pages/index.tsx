ad7c519996541c379ba976498c20e0854a49047d
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-07-07 20:10:18 -0300

    Feat/Building Frontend with Shakra-Ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import Input from '../components/Form/Input';
+
+const SignIn: NextPage = () => {
   return (
-    <h1>Hello World!</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth="360px"
+        bg={'gray.800'}
+        p={8}
+        borderRadius={8}
+        flexDir='column'
+      >
+        <Stack spacing={4}>
+          <Input type='email' name='email' label='E-mail' />
+          <Input type='password' name='password' label='Password' />
+        </Stack>
+
+        <Button
+          type='submit'
+          mt={6}
+          colorScheme='pink'
+          size={'lg'}
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
 
-export default Home
+export default SignIn
